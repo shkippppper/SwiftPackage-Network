a36d458d6ae50738d0476e7600065ab69ee3c15d
Akaki <a.titberidze@yahoo.com>
2024-05-03 02:32:16 +0400

    Initial Commit

--- Sources/NetworkingPackage/NetworkingPackage.swift
+++ Sources/NetworkingPackage/NetworkingPackage.swift
@@ -1,2 +1,28 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+// NetworkingPackage for Swift
+//
+// Akaki da igi
+
+import Foundation
+
+public enum NetworkError: Error {
+    case badRequest
+    case decodingError
+}
+
+public class NetworkManager {
+    public init() {}
+    
+    public func fetchData<T: Codable>(url: URL, parse: @escaping (Data) -> T?, completion: @escaping (Result<T?, NetworkError>) -> Void) {
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil,
+                  (response as? HTTPURLResponse)?.statusCode == 200
+            else {
+                completion(.failure(NetworkError.decodingError))
+                return
+            }
+            let result  = parse(data)
+            completion(.success(result))
+        }.resume()
+    }
+}
